3315f8078c9ecc8c419630cf2ae77c5dd7b4e619
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-30 16:34:03 -0400

    undo

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
+    try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err);
+        process.exit(1); // exit the Node.js process with a non-zero exit code
+    }
+}
 
 module.exports = connectDB;
