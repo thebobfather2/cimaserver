8d449a93c1124bef487f8be58ec04148b5f66b97
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-30 17:43:38 -0400

    removed fields

--- model/User.js
+++ model/User.js
@@ -2,35 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  profilePicture: {
-    type: String,
-    required: true
-  },
-  bio: {
-    type: String,
-    required: true
-  },
-  roles: {
-    User: {
-      type: Number,
-      default: 2001
+    username: {
+        type: String,
+        required: true
     },
-    Editor: Number,
-    Admin: Number
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  refreshToken: String
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        Editor: Number,
+        Admin: Number
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    refreshToken: String
 });
 
 module.exports = mongoose.model('User', userSchema);
